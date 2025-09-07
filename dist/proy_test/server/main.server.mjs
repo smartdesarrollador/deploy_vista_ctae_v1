4a7fcbcb905745e351d83b5b005a13726abdaca9
Jeans <sistemadesignstyle@gmail.com>
2025-09-07 13:58:40 -0500

    corrigiendo responsive

--- dist/proy_test/server/main.server.mjs
+++ dist/proy_test/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PULVMGRM.mjs";import"./chunk-5GX57OKY.mjs";import"./chunk-M6R3PMLX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SE5C7F3N.mjs";import"./chunk-5GX57OKY.mjs";import"./chunk-M6R3PMLX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
